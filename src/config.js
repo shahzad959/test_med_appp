a7e9991cfab4a260ce9113de115385d9b81bcab6
shahzad959 <shahzad_chauhan@hotmail.com>
2025-02-27 08:09:59 -0500

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = window.location.hostname === "localhost" ? "<add your theia server side url>" : "add your theia server side url";
+export const API_URL = window.location.hostname === "localhost" ? "https://labs-mongo-loose-damp-sandwich.mongo.databases.labs.skills.network" : "https://labs-mongo-loose-damp-sandwich.mongo.databases.labs.skills.network";
 console.log(
     "API_URL :",
     API_URL
